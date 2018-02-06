3088b717251fdd7c9ee9e47c31a27a71b839d415
grrrero <guerrewrong@gmail.com>
2018-02-06 14:53:03 -0500

    SNOW-10: Pulled from master and merged functions.php

--- functions.php
+++ functions.php
@@ -10,94 +10,93 @@ function snow_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'snow_theme_enqueue_styles' );
 
-/* Add SNOW introduction to front page */
-register_sidebar( array(
-    'name' => __( 'Snow Home', 'snow' ),
-    'id' => 'snow-home',
-    'before_widget' => '<div id="snow-home" class="snow-home">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-title">',
-    'after_title' => '</h1>'
-));
 
-/* Add 'Accessing the Site' to front page */
-register_sidebar( array(
-    'name' => __( 'Accessing the Site', 'access' ),
-    'id' => 'access-site',
-    'before_widget' => '<div id="snow-access" class="snow-access">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-access-title">',
-    'after_title' => '</h1>'
-));
+/* Centralised array for registering widgets and sidebars (post and page) on the SNOW webpage */
+$snow_widgets = array(
+    'snow-home' => array(
+        'name' => __( 'Snow Home', 'snow' ),
+        'id' => 'snow-home',
+        'before_widget' => '<div id="snow-home" class="snow-home">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-title">',
+        'after_title' => '</h1>'
+    ),
+    'access-site' => array(
+        'name' => __( 'Accessing the Site', 'access' ),
+        'id' => 'access-site',
+        'before_widget' => '<div id="snow-access" class="snow-access">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-access-title">',
+        'after_title' => '</h1>'
+    ),
+    'snow-experiences' => array(
+        'name' => __( 'Experiences', 'experiences' ),
+        'id' => 'snow-experiences',
+        'before_widget' => '<div id="snow-experiences" class="snow-experiences">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-experiences-title">',
+        'after_title' => '</h1>'
+    ),
+    'license-info' => array(
+        'name' => __( 'License Information', 'license-info' ),
+        'id' => 'license-info',
+        'before_widget' => '<div id="snow-license" class="small-12 medium-4 snow-license">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-license-title">',
+        'after_title' => '</h1>'
+    ),
+    'contact' => array(
+        'name' => __( 'Contact Information', 'contact' ),
+        'id' => 'contact',
+        'before_widget' => '<div id="snow-contact" class="small-12 medium-4 snow-contact">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-contact-title">',
+        'after_title' => '</h1>'
+    ),
+    'partners' => array(
+        'name' => __( 'Partners', 'partners' ),
+        'id' => 'partners',
+        'before_widget' => '<div id="snow-partners" class="small-12 medium-4 snow-partners">',
+        'after_widget' => '</div>',
+        'before_title' => '<h1 class="snow-partners-title">',
+        'after_title' => '</h1>'
+    ),
+    'post',
+    'page'
+);
 
-/* Add 'Share your experiences on SNOW' to front page.' */
-register_sidebar( array(
-    'name' => __( 'Experiences', 'experiences' ),
-    'id' => 'snow-experiences',
-    'before_widget' => '<div id="snow-experiences" class="snow-experiences">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-experiences-title">',
-    'after_title' => '</h1>'
-));
-
-/* Add footer widgets */
-register_sidebar( array(
-    'name' => __( 'License Information', 'license-info' ),
-    'id' => 'license-info',
-    'before_widget' => '<div id="snow-license" class="small-12 medium-4 snow-license">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-license-title">',
-    'after_title' => '</h1>'
-));
-
-register_sidebar( array(
-    'name' => __( 'Contact Information', 'contact' ),
-    'id' => 'contact',
-    'before_widget' => '<div id="snow-contact" class="small-12 medium-4 snow-contact">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-contact-title">',
-    'after_title' => '</h1>'
-));
-
-register_sidebar( array(
-    'name' => __( 'Partners', 'partners' ),
-    'id' => 'partners',
-    'before_widget' => '<div id="snow-partners" class="small-12 medium-4 snow-partners">',
-    'after_widget' => '</div>',
-    'before_title' => '<h1 class="snow-partners-title">',
-    'after_title' => '</h1>'
-));
-/* End footer widgets */
+/* Register post/page sidebars */
+foreach ($snow_widgets as $value) {
+    register_sidebar($value);
+}
 
 
 /* Begin extending widget for Upcoming Workshops front panel */
-function snow_front_panel_sticky( $atts = array() ) { 
- 
+function snow_front_panel_sticky( $atts = array() ) {
     /* Default Parameters */
     extract(shortcode_atts(array(
-      'category__and' => 7,
-      'posts_per_page' => 1,
-      'post__in' => get_option( 'sticky_posts' ),
-      'ignore_sticky_posts' => 1
+        'category__and' => 7,
+        'posts_per_page' => 1,
+        'post__in' => get_option( 'sticky_posts' ),
+        'ignore_sticky_posts' => 1
     ), $atts));
-     
+    
     /* Query the posts */
     $the_query = new WP_Query($atts);
     if ( $the_query->have_posts() ) {
         while ( $the_query->have_posts() ) {
             $the_query->the_post();
-            $return .= '<p><a href="' .get_permalink(). '" title="'  . get_the_title() . '">' . get_the_title() . '</a></p>' . '<p>' . get_the_excerpt() . '</p>';
+            $return .= '<p><a href="' .get_permalink(). '" title="' . get_the_title() . '">' . get_the_title() . '</a></p>' . '<p>' . get_the_excerpt() . '</p>';
         }
-     
     } else {
         // No posts found
     }
 
     /* Restore original Post Data */
     wp_reset_postdata();
-     
-    return $return; 
-} 
+    
+    return $return;
+}
 add_shortcode('snow_front_panel_sticky', 'snow_front_panel_sticky');
 
 
@@ -105,81 +104,80 @@ class snow_panel_widget extends WP_Widget {
 
     public $title;
     public $category;
-
+    
     /* Set up the widget name and description */
     public function __construct( $id, $title, $category ) {
         $widget_options = array();
         $this->title = $title;
         $this->category = $category;
         parent::__construct( $id, $title, $widget_options );
-    }    
-
+    }
+    
     /* Create the widget output */
     public function widget( $args ) {
-
         echo $args['before_widget'] . $args['before_title'] . $this->title . $args['after_title']; ?>
         <div class="snow-widget">
             <?php echo do_shortcode('[snow_front_panel_sticky category__and=' . $this->category . ']'); ?>
         </div>
         <?php echo $args['after_widget'];
     }
-
 }
 
-function snow_upcoming_workshops() { 
-  register_widget( new snow_panel_widget('snow_upcoming_workshops','Upcoming Workshops','8'));
-}
-add_action('widgets_init', 'snow_upcoming_workshops');
+/* Centralised array for the SNOW panel information */
+$snow_panels = array(
+    'snow_upcoming_workshops' => array(
+        'id' => 'snow_upcoming_workshops',
+        'title' => 'Upcoming Workshops',
+        'category' => '8'
+    ),
+    'snow_feature_article' => array(
+        'id' => 'snow_feature_article',
+        'title' => 'Feature Article',
+        'category' => '22'
+    ),
+    'snow_featured_content' => array(
+        'id' => 'snow_featured_content',
+        'title' => 'Featured Content',
+        'category' => '23'
+    )
+);
 
-function snow_feature_article() { 
-    register_widget( new snow_panel_widget('snow_feature_article','Feature Article','22'));
+/* Register and initialise all panels in $snow_panels */
+foreach ($snow_panels as $panel) {
+    register_widget( new snow_panel_widget($panel['id'], $panel['title'], $panel['category']) );
+    add_action('widgets_init', $panel['id']);
 }
-add_action('widgets_init', 'snow_feature_article');
-
-function snow_featured_content() { 
-    register_widget( new snow_panel_widget('snow_featured_content','Featured Content','23'));
-}
-add_action('widgets_init', 'snow_featured_content');
 
 /* Enable shortcodes */
 add_filter('widget_text', 'do_shortcode');
 
-
-if (function_exists('register_sidebar')) { 
-    register_sidebar('post'); 
-}
-
-if (function_exists('register_sidebar')) { 
-    register_sidebar('page'); 
-}
-
+/* Create sidebar menus for posts or pages */
 function create_sidebar($post_type) {
     $category_current = get_the_category();
-    $category_link =  get_term_link ($category_current[0]->slug, "category");
+    $category_link = get_term_link ($category_current[0]->slug, "category");
     $current_displayed_id = get_the_ID (); // the ID of the post currently displayed in the main content panel.
 
     if ( ! empty( $category_current ) ) {
         $args = array(
-            // get posts belonging to the current category.
+        // get posts belonging to the current category.
             'post_type' => $post_type,
             'category_name' => $category_current[0]->slug,
         );
         $sidebar_query = new WP_Query($args);
 
         if ($sidebar_query->have_posts()) :
-
             /*
-            If there are more than 1 post in the current category,
+            If there is more than 1 post in the current category,
             then the first item should be a link to the category.
 
-            Otherwise if there's just 1 post, then it can be ignore as it
+            Otherwise if there's just 1 post, then it can be ignored as it
             will be the landing page already being shown.
             */
             if ($sidebar_query->found_posts > 1) :?>
                 <ul>
                     <li><a href="<?php echo $category_link; ?>" class="a11y-sidebar-category-link
                 <?php
-                if (! empty ($category_landing_id)) : ?>a11y-sidebar-current<?php endif;?>">
+                    if (! empty ($category_landing_id)) : ?>a11y-sidebar-current<?php endif;?>">
                     <?php
                     /*
                     if category_landing_id is not empty, then we are showing a
@@ -191,32 +189,31 @@ function create_sidebar($post_type) {
                         <ul>
                         <?php
                         while ( $sidebar_query->have_posts() ) : $sidebar_query->the_post();
-                            /*
-                            Display all pages in the category as a list of links.
-                            But if one of the pages is the landing page, we skip it
-                            since it's already the first item in the list.
-                            */
+                        /*
+                        Display all pages in the category as a list of links.
+                        But if one of the pages is the landing page, we skip it
+                        since it's already the first item in the list.
+                        */
 
-                            $is_landing_page = get_post_meta (get_the_ID(), 'is_landing_page', true);
-                            if ( empty($is_landing_page) && (get_the_ID() != $category_landing_id)) :
-                            // If the post isn't the landing page, show a link. Otherwise ignore it.
-                                ?>
-                            <li>
-                                <a href="<?php the_permalink() ?>"
-                                <?php if ($current_displayed_id == get_the_ID() && empty ($category_landing_id)) :
-                                /*
-                                Check the category_landing_id if it is set, then we're displaying the
-                                landing page. So don't show the active styling for this item.
-                                */
-                                ?>
-                                    class="a11y-sidebar-current"
-                                    aria-current="page"
-                                <?php endif;?>>
-                                <?php the_title(); ?>
+                        $is_landing_page = get_post_meta (get_the_ID(), 'is_landing_page', true);
+                        if ( empty($is_landing_page) && (get_the_ID() != $category_landing_id)) :
+                        // If the post isn't the landing page, show a link. Otherwise ignore it.
+                        ?>
+                        <li>
+                            <a href="<?php the_permalink() ?>"
+                            <?php if ($current_displayed_id == get_the_ID() && empty ($category_landing_id)) :
+                            /*
+                            Check the category_landing_id if it is set, then we're displaying the
+                            landing page. So don't show the active styling for this item.
+                            */
+                            ?>
+                                class="a11y-sidebar-current"
+                                aria-current="page"
+                            <?php endif;?>>
+                            <?php the_title(); ?>
                                 </a>
                             </li>
                             <?php
-
                             endif;
                         endwhile;?>
                         </ul>
@@ -227,6 +224,17 @@ function create_sidebar($post_type) {
         endif;
     }
 }
+
+
+// Find out what is the post/page's main category and turn on menu indicator for that category
+function make_current_menu_item( $classes, $item ) {
+
+    if (in_array('current-post-ancestor', $classes) || in_array('current-page-ancestor', $classes) || in_array('current-menu-item', $classes) ) {
+        $classes[] = 'current-menu-item';
+    }
+    return $classes;
+}
+add_filter( 'nav_menu_css_class', 'make_current_menu_item', 10, 2 );
 
 
 // Add tag and category support to pages
@@ -240,6 +248,7 @@ function allow_pages_tags_categories() {
     }
 }
 add_action('init', 'allow_pages_tags_categories');
+
 
 // Ensure all tags and categories are included in queries
 function add_queries_tags_categories($wp_query) {
