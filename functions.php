d47b7906cc4936e95fba03b82e4e753add9d72f9
grrrero <guerrewrong@gmail.com>
2018-02-06 14:17:40 -0500

    SNOW-10: Fixed indent spacing

--- functions.php
+++ functions.php
@@ -12,60 +12,60 @@ add_action( 'wp_enqueue_scripts', 'snow_theme_enqueue_styles' );
 
 /* Add SNOW introduction to front page */
 register_sidebar( array(
-	'name' => __( 'Snow Home', 'snow' ),
-	'id' => 'snow-home',
-	'before_widget' => '<div id="snow-home" class="snow-home">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'Snow Home', 'snow' ),
+    'id' => 'snow-home',
+    'before_widget' => '<div id="snow-home" class="snow-home">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-title">',
+    'after_title' => '</h1>'
 ));
 
 /* Add 'Accessing the Site' to front page */
 register_sidebar( array(
-	'name' => __( 'Accessing the Site', 'access' ),
-	'id' => 'access-site',
-	'before_widget' => '<div id="snow-access" class="snow-access">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-access-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'Accessing the Site', 'access' ),
+    'id' => 'access-site',
+    'before_widget' => '<div id="snow-access" class="snow-access">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-access-title">',
+    'after_title' => '</h1>'
 ));
 
 /* Add 'Share your experiences on SNOW' to front page.' */
 register_sidebar( array(
-	'name' => __( 'Experiences', 'experiences' ),
-	'id' => 'snow-experiences',
-	'before_widget' => '<div id="snow-experiences" class="snow-experiences">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-experiences-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'Experiences', 'experiences' ),
+    'id' => 'snow-experiences',
+    'before_widget' => '<div id="snow-experiences" class="snow-experiences">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-experiences-title">',
+    'after_title' => '</h1>'
 ));
 
 /* Add footer widgets */
 register_sidebar( array(
-	'name' => __( 'License Information', 'license-info' ),
-	'id' => 'license-info',
-	'before_widget' => '<div id="snow-license" class="small-12 medium-4 snow-license">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-license-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'License Information', 'license-info' ),
+    'id' => 'license-info',
+    'before_widget' => '<div id="snow-license" class="small-12 medium-4 snow-license">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-license-title">',
+    'after_title' => '</h1>'
 ));
 
 register_sidebar( array(
-	'name' => __( 'Contact Information', 'contact' ),
-	'id' => 'contact',
-	'before_widget' => '<div id="snow-contact" class="small-12 medium-4 snow-contact">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-contact-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'Contact Information', 'contact' ),
+    'id' => 'contact',
+    'before_widget' => '<div id="snow-contact" class="small-12 medium-4 snow-contact">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-contact-title">',
+    'after_title' => '</h1>'
 ));
 
 register_sidebar( array(
-	'name' => __( 'Partners', 'partners' ),
-	'id' => 'partners',
-	'before_widget' => '<div id="snow-partners" class="small-12 medium-4 snow-partners">',
-	'after_widget' => '</div>',
-	'before_title' => '<h1 class="snow-partners-title">',
-	'after_title' => '</h1>'
+    'name' => __( 'Partners', 'partners' ),
+    'id' => 'partners',
+    'before_widget' => '<div id="snow-partners" class="small-12 medium-4 snow-partners">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="snow-partners-title">',
+    'after_title' => '</h1>'
 ));
 /* End footer widgets */
 
@@ -73,56 +73,56 @@ register_sidebar( array(
 /* Begin extending widget for Upcoming Workshops front panel */
 function snow_front_panel_sticky( $atts = array() ) { 
  
- 	/* Default Parameters */
-	extract(shortcode_atts(array(
-	  'category__and' => 7,
-	  'posts_per_page' => 1,
-	  'post__in' => get_option( 'sticky_posts' ),
-	  'ignore_sticky_posts' => 1
-	), $atts));
-	 
-	/* Query the posts */
-	$the_query = new WP_Query($atts);
-	if ( $the_query->have_posts() ) {
-	    while ( $the_query->have_posts() ) {
-	        $the_query->the_post();
-	        $return .= '<p><a href="' .get_permalink(). '" title="'  . get_the_title() . '">' . get_the_title() . '</a></p>' . '<p>' . get_the_excerpt() . '</p>';
-	    }
+    /* Default Parameters */
+    extract(shortcode_atts(array(
+      'category__and' => 7,
+      'posts_per_page' => 1,
+      'post__in' => get_option( 'sticky_posts' ),
+      'ignore_sticky_posts' => 1
+    ), $atts));
      
-	} else {
-	    // No posts found
-	}
+    /* Query the posts */
+    $the_query = new WP_Query($atts);
+    if ( $the_query->have_posts() ) {
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            $return .= '<p><a href="' .get_permalink(). '" title="'  . get_the_title() . '">' . get_the_title() . '</a></p>' . '<p>' . get_the_excerpt() . '</p>';
+        }
+     
+    } else {
+        // No posts found
+    }
 
-	/* Restore original Post Data */
-	wp_reset_postdata();
-	 
-	return $return; 
+    /* Restore original Post Data */
+    wp_reset_postdata();
+     
+    return $return; 
 } 
 add_shortcode('snow_front_panel_sticky', 'snow_front_panel_sticky');
 
 
 class snow_panel_widget extends WP_Widget {
 
-	public $title;
-	public $category;
+    public $title;
+    public $category;
 
-	/* Set up the widget name and description */
-	public function __construct( $id, $title, $category ) {
-		$widget_options = array();
-		$this->title = $title;
-		$this->category = $category;
-		parent::__construct( $id, $title, $widget_options );
-	}    
+    /* Set up the widget name and description */
+    public function __construct( $id, $title, $category ) {
+        $widget_options = array();
+        $this->title = $title;
+        $this->category = $category;
+        parent::__construct( $id, $title, $widget_options );
+    }    
 
-	/* Create the widget output */
-	public function widget( $args ) {
+    /* Create the widget output */
+    public function widget( $args ) {
 
-	    echo $args['before_widget'] . $args['before_title'] . $this->title . $args['after_title']; ?>
-	   	<div class="snow-widget">
-	 		<?php echo do_shortcode('[snow_front_panel_sticky category__and=' . $this->category . ']'); ?>
-	    </div>
-	    <?php echo $args['after_widget'];
-	}
+        echo $args['before_widget'] . $args['before_title'] . $this->title . $args['after_title']; ?>
+        <div class="snow-widget">
+            <?php echo do_shortcode('[snow_front_panel_sticky category__and=' . $this->category . ']'); ?>
+        </div>
+        <?php echo $args['after_widget'];
+    }
 
 }
 
@@ -132,12 +132,12 @@ function snow_upcoming_workshops() {
 add_action('widgets_init', 'snow_upcoming_workshops');
 
 function snow_feature_article() { 
-	register_widget( new snow_panel_widget('snow_feature_article','Feature Article','22'));
+    register_widget( new snow_panel_widget('snow_feature_article','Feature Article','22'));
 }
 add_action('widgets_init', 'snow_feature_article');
 
 function snow_featured_content() { 
-	register_widget( new snow_panel_widget('snow_featured_content','Featured Content','23'));
+    register_widget( new snow_panel_widget('snow_featured_content','Featured Content','23'));
 }
 add_action('widgets_init', 'snow_featured_content');
 
@@ -231,26 +231,26 @@ function create_sidebar($post_type) {
 
 // Add tag and category support to pages
 function allow_pages_tags_categories() {
-	$taxonomy = array(
-		"post_tag",
-		"category"
-	);
-	foreach ($taxonomy as $value) {
-		register_taxonomy_for_object_type($value, 'page');
-	}
+    $taxonomy = array(
+        "post_tag",
+        "category"
+    );
+    foreach ($taxonomy as $value) {
+        register_taxonomy_for_object_type($value, 'page');
+    }
 }
 add_action('init', 'allow_pages_tags_categories');
 
 // Ensure all tags and categories are included in queries
 function add_queries_tags_categories($wp_query) {
-	$query_parameters = array(
-		"tag",
-		"category_name"
-	);
-	foreach ($query_parameters as $value) {
-		if ($wp_query->get($value))
-			$wp_query->set('post_type', 'any');
-	}
+    $query_parameters = array(
+        "tag",
+        "category_name"
+    );
+    foreach ($query_parameters as $value) {
+        if ($wp_query->get($value))
+            $wp_query->set('post_type', 'any');
+    }
 }
 add_action('pre_get_posts', 'add_queries_tags_categories');
 
