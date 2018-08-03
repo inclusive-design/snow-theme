0a959d2f5630d1e54dcc09bfda23745b383a616b
jhung <jhung@ocadu.ca>
2018-08-03 11:18:55 -0400

    SNOW-36: Changed behaviour of sidebar
    Sidebar is generated exclusively through Page content and shows
    immediate parent and immediate children only (if available).

--- functions.php
+++ functions.php
@@ -172,80 +172,49 @@ add_filter('widget_text', 'do_shortcode');
 
 /* Create sidebar menus for posts or pages */
 function create_sidebar($post_type) {
-    // Get currently displayed category ID as defined in category.php
-    global $category_landing_id;
-    
-    $category_current = get_the_category();
-    $category_link = get_term_link ($category_current[0]->slug, "category");
-    $current_displayed_id = get_the_ID (); // the ID of the post currently displayed in the main content panel.
+    $output = '';
+    $current_displayed_id = get_the_ID ();
 
-    if ( ! empty( $category_current ) ) {
-        $args = array(
-        // get posts belonging to the current category.
-            'post_type' => $post_type,
-            'posts_per_page'=>-1,
-            'category_name' => $category_current[0]->slug,
-        );
-        $sidebar_query = new WP_Query($args);
+    // Get all immediate child pages.
+    $pages = get_pages (array (
+        'parent'=> $current_displayed_id,
+        'sort_column' => 'menu_order'
+    ));
 
-        if ($sidebar_query->have_posts()) :
-            /*
-            If there is more than 1 post in the current category,
-            then the first item should be a link to the category.
+    // Get the immediate parent
+    $parent_id = wp_get_post_parent_id();
 
-            Otherwise if there's just 1 post, then it can be ignored as it
-            will be the landing page already being shown.
-            */
-            if ($sidebar_query->found_posts > 1) :?>
-                <ul>
-                    <li><a href="<?php echo $category_link; ?>" class="a11y-sidebar-category-link
-                <?php
-                    if (! empty ($category_landing_id)) : ?>a11y-sidebar-current<?php endif;?>">
-                    <?php
-                    /*
-                    if category_landing_id is not empty, then we are showing a
-                    landing page. Therefore give it active styling.
-                    */
-                    echo $category_current[0]->name; ?></a></li>
-
-                    <li>
-                        <ul>
-                        <?php
-                        while ( $sidebar_query->have_posts() ) : $sidebar_query->the_post();
-                        /*
-                        Display all pages in the category as a list of links.
-                        But if one of the pages is the landing page, we skip it
-                        since it's already the first item in the list.
-                        */
-
-                        $is_landing_page = get_post_meta (get_the_ID(), 'is_landing_page', true);
-                        if ( empty($is_landing_page) && (get_the_ID() != $category_landing_id)) :
-                        // If the post isn't the landing page, show a link. Otherwise ignore it.
-                        ?>
-                        <li>
-                            <a href="<?php the_permalink() ?>"
-                            <?php if ($current_displayed_id == get_the_ID() && empty ($category_landing_id)) :
-                            /*
-                            Check the category_landing_id if it is set, then we're displaying the
-                            landing page. So don't show the active styling for this item.
-                            */
-                            ?>
-                                class="a11y-sidebar-current"
-                                aria-current="page"
-                            <?php endif;?>>
-                            <?php the_title(); ?>
-                                </a>
-                            </li>
-                            <?php
-                            endif;
-                        endwhile;?>
-                        </ul>
-                    </li>
-                </ul>
-                <?php
-            endif;
-        endif;
+    if ($pages && $parent_id) {
+        // If somewhere in the middle of a Pages branch (there are parent and children Pages)
+        // then show a link back to the immediate parent, and links to immediate children.
+        $output .= '<ul>';
+        $output .= '<li><a href="'. get_permalink($parent_id).'">Back to: '.get_post ($parent_id)->post_title.'</a></li>';
+        $output .= '<li><ul>';
+        $output .= '<li>'.get_the_ID ().'<a href="'. get_page_link() .'" class="a11y-sidebar-current" aria-current="page">'. get_the_title() .'</a></li>';
+        $output .= '<li><ul>';
+        foreach ( $pages as $page ) {
+            $output.= '<li><a href="'.get_page_link ($page->ID).'">'.$page->post_title.'</a></li>';
+        }
+        $output .= '</ul></li></ul></li></ul>';
+    } else if ($pages) {
+        // If at the top of the Pages tree (there is no parent), then list all the immediate children.
+        $output .= '<ul>';
+        $output .= '<li>'.get_the_ID ().'<a href="'. get_page_link() .'" class="a11y-sidebar-current" aria-current="page">'. get_the_title() .'</a></li>';
+        $output .= '<li><ul>';
+        foreach ( $pages as $page ) {
+            $output.= '<li><a href="'.get_page_link ($page->ID).'">'.$page->post_title.'</a></li>';
+        }
+        $output .= '</ul></li></ul>';
+    } else if ($parent_id) {
+        // If at the end of a Pages branch (there is parent but no more children),
+        // then show a link back to immediate parent, and the current page.
+        $output .= '<ul>';
+        $output .= '<li><a href="'. get_permalink($parent_id).'">Back to: '.get_post ($parent_id)->post_title.'</a></li>';
+        $output .= '<li><ul>';
+        $output .= '<li>'.get_the_ID ().'<a href="'. get_page_link() .'" class="a11y-sidebar-current" aria-current="page">'. get_the_title() .'</a></li>';
+        $output .= '</ul></li></ul>';
     }
+    echo $output;
 }
 
 // Find out what is the post/page's main category and turn on menu indicator for that category
@@ -369,9 +338,8 @@ function custom_breadcrumbs() {
                 $anc = array_reverse($anc);
 
                 // Parent page loop
-                // if ( !isset( $parents ) ) $parents = null;
                 foreach ( $anc as $ancestor ) {
-                    $output .= '<li><a href="' . get_permalink($ancestor) . '>' . get_the_title($ancestor) . '</a></li>';
+                    $output .= '<li><a href="' . get_permalink($ancestor) . '">' . get_the_title($ancestor) . '</a></li>';
                 }
 
                 // Current page
